cbf9cd0554732e6efaa0696590869929d9ddf699
UNIMAG\ernestojarabajd <ejarabad@gmail.com>
2023-04-13 12:54:41 -0500

    Agregando servicio de correo electronico para las alertas

--- Server/Services/analitica/analitica.py
+++ Server/Services/analitica/analitica.py
@@ -4,16 +4,32 @@ import csv
 import pandas as pd
 from sklearn.linear_model import LinearRegression
 import numpy as np
+import smtplib
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import mean_squared_error, r2_score
 from influxdb_client.client.write_api import SYNCHRONOUS
+import os; import locale;  os.environ["PYTHONIOENCODING"] = "utf-8";
+myLocale=locale.setlocale(category=locale.LC_ALL, locale="en_GB.UTF-8");
 from influxdb_client import InfluxDBClient, Point, WritePrecision
+
+# Configura los parámetros del correo electrónico
+remitente = 'lasama1525@gmail.com'
+password = 'znxtmpcebromjqga'
+destinatario = 'lasama1525@gmail.com'
+asunto = 'Prueba'
+cuerpo = 'Cuerpo del correo electrónico'
+
+# Configura el servidor SMTP y el puerto a utilizar
+server = smtplib.SMTP('smtp.gmail.com', 587)
+server.starttls()
+
+# Inicia sesión en el servidor SMTP
+server.login(remitente, password)
 
 token = "token-secreto"
 org = "org"
 bucket = "RabbitBucket"
 client1 = InfluxDBClient(url="http://4.204.226.150:8086", token=token)
-datos = []
 pot = []
 
 
@@ -21,14 +37,11 @@ pot = []
 # Define las variables que se usarán para guardar los mensajes
 mensaje_1 = '' #Valor voltaje
 mensaje_2 = '' #Valor Corriente
-mensaje_3 = '' #Valor potencia
+mensaje_3 = 0 #Valor potencia
 costo = 800
+datos = []
 promPot = 0
-costoEnergia= 0
-
-
-
-
+tiempo_inicial = time.time() # guarda el tiempo actual
 
 # Abre el archivo CSV en modo de escritura y define el encabezado
 with open('datos.csv', 'w', newline='') as archivo_csv:
@@ -61,22 +74,34 @@ client.subscribe('potencia')
 # Inicia el loop del cliente MQTT
 client.loop_start()
 
-"""     for element, i in datos:
-        if datos[i] != '':
-            pot.append(float(element))  """
+""" for i in range(len(datos)):
+        pot.append(float(datos[i])) """
 
-# Bucle infinito que se ejecutará cada 5 segundos para mostrar los mensajes
+# Bucle infinito que se ejecutará cada 2 segundos para mostrar los mensajes
 while True:
+    tiempo_transcurrido = time.time() - tiempo_inicial
+
     datos.append(mensaje_3)
-    print(datos)
+    pot = ([float(i) for i in datos if i != 0])
+    print('lista potencia:', pot)
 
-    for i in dir(datos):
-        if datos[i] != '':
-            pot.append(float(datos[i]))
+    promPot = (sum(pot)/len(datos))/1000
+    costoEnergia = ((sum(pot)/1000) * 0.016667) * costo
+    print('costo energia:',costoEnergia)
 
-    promedioPot = sum(pot)/len(datos)
+    for i in range(len(pot)):
+        if(pot[i] == 0):
+             # Crea el mensaje a enviar
+            mensaje = ''
 
-    print('Promedio potencia:', promedioPot)
+            # Envía el mensaje
+            server.sendmail(remitente, destinatario, 'No hay consumo de potencia')
+
+            """ # Cierra la conexión con el servidor SMTP
+            server.quit() """
+       
+
+    print('promedio potencia:', promPot)
     # Abre el archivo CSV en modo de añadir para agregar los nuevos datos
     with open('datos.csv', 'a', newline='') as archivo_csv:
         writer = csv.writer(archivo_csv)
@@ -86,6 +111,7 @@ while True:
 
         archivo_csv = pd.read_csv('datos.csv')
         archivo_csv= archivo_csv.dropna()
+        
         total_rows=len(archivo_csv.axes[0])
         if total_rows > 10:
             Datos_numericos = ['promedioPotencia', 'costo']
@@ -99,19 +125,28 @@ while True:
 
             # Fit a linear regression model on the training set
             model = LinearRegression().fit(X_train, y_train)
-            predictions = model.predict(X_test)
-            np.set_printoptions(suppress=True)
-            mse = mean_squared_error(y_test, predictions)
-            rmse = np.sqrt(mse)
-            r2 = r2_score(y_test, predictions)
-        
+            if tiempo_transcurrido >= 60:
 
-            write_api = client1.write_api(write_options=SYNCHRONOUS)
+                with open('datos.csv', 'a', newline='') as archivo_csv:
+                    writer = csv.writer(archivo_csv)
+                    writer.writerow([promPot,costo, costoEnergia])
 
-            # Crear punto de datos
-            point = Point("Analitica").tag("ubicacion", "cuarto").field("prediccion", predictions[0]).field("mse", mse).field("rmse", rmse).field("r2", r2)
+                pot.clear()
+                datos.clear()
+                
+                predictions = model.predict(X_test)
+                np.set_printoptions(suppress=True)
 
-            # Escribir los datos en la base de datos
-            write_api.write(bucket=bucket, org=org, record=point)
+                write_api = client1.write_api(write_options=SYNCHRONOUS)
 
-    time.sleep(5)    
+                # Crear punto de datos
+                point = Point("Analitica").tag("ubicacion", "cuarto").field("prediccion", predictions[0]).field("promedioPotencia", promPot)
+
+                # Escribir los datos en la base de datos
+                write_api.write(bucket=bucket, org=org, record=point)
+                with open('datos.csv', 'w', newline='') as archivo_csv:
+                    writer = csv.writer(archivo_csv)
+                    writer.writerow(['promedioPotencia', 'costo', 'costoEnergia'])
+                tiempo_inicial = time.time()
+
+    time.sleep(2)    
